6c97642b7cba0b479889521f6eb4ad85c4af1b7c
DiegoG <80786405+Skapebreak@users.noreply.github.com>
2022-02-22 22:32:45 -0700

    responsive template completed

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ const App = () => (
     <Header />
     <AboutUs />
     <SpecialMenu />
-    {/* <Chef />
-    <Intro />
+    <Chef />
+    {/* <Intro /> */}
     <Laurels />
     <Gallery />
     <FindUs />
-    <Footer /> */}
+    <Footer />
   </div>
 );
 
